031bfa7ee40d249a183c0a2df333422070972c31
Arajit Samanta <arijit24@gmail.com>
2023-03-25 11:47:46 -0500

    common concepts

---
+++ common_concepts/src/consts.rs
@@ -0,0 +1,3 @@
+pub fn constants(){
+    const THREE_HR_IN_SEC: u32 = 60 *60 * 3;
+}
---
+++ common_concepts/src/main.rs
@@ -0,0 +1,8 @@
+mod immutability;
+mod consts;
+
+fn main() {
+    println!("Hello, world!");
+    immutability::immutability();
+    consts::constants();
+}
